77b30faf6e479a5fe92cf582f487fb7872fe814f
renandcr <renandamagazine@gmail.com>
2022-01-25 00:41:14 -0300

    Atualizado

--- script.js
+++ script.js
@@ -1,71 +1,66 @@
 class Traveler{
-    constructor(nome){
-        this.nome = nome
+    constructor(name){
+        this._name = name
         this.food = 1
         this.isHealthy = true
-
     }
-    
+
     hunt(){
-        if(this.hunt){
-            this.food += 2
-        }    
+        return this.food += 2
     }
 
     eat(){
         if(this.food > 0){
-            this.food -= 1
+            return this.food -= 1
         } else{
-            this.isHealthy = false
+            return this.isHealthy = false
         }
     }
 }
+
+const travele1 = new Traveler("Renan")
+// travele1.hunt()
+travele1.eat()
+travele1.eat()
+// console.log(travele1)
 
 class Wagon{
     constructor(capacity){
-        this.capacity = capacity
-        this.travelersList = []
-        this.emptySeats = 0
+        this._capacity = capacity
+        this.passengers = []
     }
 
     getAvailableSeatCount(){
-        return this.capacity - this.travelersList.length 
+        return this._capacity - this.passengers.length
     }
 
-    join(passenger){
+    join(traveler){
         if(this.getAvailableSeatCount() > 0){
-            this.travelersList.push(passenger)
+            return this.passengers.push(traveler)
         } else{
-            this.travelersList.push()
+            this.passengers.push()
         }
     }
-
     shouldQuarantine(){
         let result = 0
-        for(let i = 0; i < this.travelersList.length; i++){
-            if(this.travelersList[i].isHealthy === false){
-                result = true
+        const find = this.passengers.find(function(current){
+            if(current.isHealthy == false){
+                return result = true
+            } else{
+                return result = false
             }
-        }
+        })
         return result
     }
-
     totalFood(){
         let result = 0
-        for(let i = 0; i < this.travelersList.length; i++){
-            if(this.travelersList[i].food){
-                result += this.travelersList[i].food
-            }
-        }
+        const filter = this.passengers.filter(function(current){
+            return result += current.food
+        })
         return result
     }
-    
 }
 
-
-
-
-////////////////////////////////////
 
 // Criar uma carroça que comporta 2 pessoas
 let wagon = new Wagon(2);
@@ -75,17 +70,17 @@ let juan = new Traveler('Juan');
 let maude = new Traveler('Maude');
 
 console.log(`${wagon.getAvailableSeatCount()} should be 2`);
+
 wagon.join(henrietta);
 console.log(`${wagon.getAvailableSeatCount()} should be 1`);
 
 wagon.join(juan);
 wagon.join(maude); // Não tem espaço para ela!
 console.log(`${wagon.getAvailableSeatCount()} should be 0`);
-henrietta.hunt(); // pega mais comida
 
+henrietta.hunt(); // pega mais comida
 juan.eat();
 juan.eat(); // juan agora está com fome (doente)
-
 
 console.log(`${wagon.shouldQuarantine()} should be true since juan is sick`);
 console.log(`${wagon.totalFood()} should be 3`);
